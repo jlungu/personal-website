4d29fddca057cd9e76037be90c05b5f5e96cb9ed
U-James-PC\James <jamesjjlungu@gmail.com>
2020-03-27 00:39:09 -0400

    First commit. Added about and home pages

---
+++ src/Components/Projects.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Projects extends Component{
+    render(){
+        return(
+            <section id="projects">
+                <div className="row">
+                    
+                </div>
+            </section>
+        );
+    }
+}
+export default Projects;
