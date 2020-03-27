e1a94686a2dcc232736fe942b65cbdafa13431d5
U-James-PC\James <jamesjjlungu@gmail.com>
2020-03-27 12:56:43 -0400

    Added my projects tab. Started incorperating twitter bootstrap, since materialize was being difficult.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,31 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Header extends Component {
   render() {
-
-    if(this.props.data){
+    if (this.props.data) {
       var name = this.props.data.name;
-      var occupation= this.props.data.occupation;
-      var description= this.props.data.description;
-      var city= this.props.data.address.city;
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
+      var occupation = this.props.data.occupation;
+      var description = this.props.data.description;
+      var city = this.props.data.address.city;
+      var networks = this.props.data.social.map(function(network) {
+        return (
+          <li key={network.name}>
+            <a href={network.url}>
+              <i className={network.className}></i>
+            </a>
+          </li>
+        );
+      });
     }
     let headerStyle = {
-       fontSize:"32px",
-       color: "white"
-    }
+      fontSize: "32px",
+      color: "white"
+    };
 
     return (
       <header id="home">
-
-      <nav id="nav-wrap">
+        <nav id="nav-wrap">
 
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
-         <ul id="nav" className="nav">
+         <ul id="nav" className="navz">
             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
             <li><a className="smoothscroll" href="#about">About</a></li>
             <li><a className="smoothscroll" href="#projects">Projects</a></li>
@@ -33,23 +37,27 @@ class Header extends Component {
 
       </nav>
 
-      <div className="row banner">
-         <div className="banner-text">
+
+        <div className="row banner">
+          <div className="banner-text">
             <h1 className="responsive-headline">Hey There.</h1>
-            <h2 id="name-header" style={headerStyle}>This is {name}.</h2>
-            <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+            <h4 id="name-header" style={headerStyle}>
+              This is {name}.
+            </h4>
+            <h3>
+              I'm a {city} based <span>{occupation}</span>. {description}.
+            </h3>
             <hr />
-            <ul className="social">
-               {networks}
-            </ul>
-         </div>
-      </div>
+            <ul className="social">{networks}</ul>
+          </div>
+        </div>
 
-      <p className="scrolldown">
-         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-      </p>
-
-   </header>
+        <p className="scrolldown">
+          <a className="smoothscroll" href="#about">
+            <i className="icon-down-circle"></i>
+          </a>
+        </p>
+      </header>
     );
   }
 }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,14 +1,69 @@
-import React, { Component } from 'react';
-
-class Projects extends Component{
-    render(){
-        return(
-            <section id="projects">
-                <div className="row">
-                    
-                </div>
-            </section>
-        );
+import React, { Component } from "react";
+import { Card, Icon, CardTitle, Modal, Button } from "react-materialize";
+class Projects extends Component {
+  render() {
+    let wireframer = {  
+      marginBottom: "24px",
     }
+    let press = {
+      marginTop: "-2px"
+    }
+    let project = {
+      marginBottom: "45px",
+    }
+    return (
+      <section id="projects">
+        <h1 className=" project-header">My Projects</h1>
+        <h3 className="project-h3">Here are a few of the most recent projects I have been working on</h3>
+          <div class="row row-cols-1 row-cols-md-2">
+            <div class="col mb-4">
+              <div class="card h-100">
+                <img src="images/wireframer.png" class="card-img-top" alt="..." style={wireframer} />
+                <div class="card-body">
+                  <h5 class="card-title">Wireframer App</h5>
+                  <p class="card-text project-text">
+                  Wireframer Web App. Coded with JavaScript, React.js, Redux
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col mb-4">
+              <div class="card h-100">
+                <img src="images/todolist.png" class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">To-Do List</h5>
+                  <p class="card-text project-text">
+                  To-Do List Manager.  Coded with ReactJS, JavaScript
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col mb-4">
+              <div class="card h-100">
+                <img src="images/instructions.png" class="card-img-top"/>
+                <div class="card-body">
+                  <h5 class="card-title" style={press}>Press Release Doc</h5>
+                  <p class="card-text project-text">
+                  Mock Press Release, created for Technical Communications Class.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col mb-4">
+              <div class="card h-100">
+                <img src="images/resources.png" class="card-img-top" style={project} />
+                <div class="card-body">
+                  <h5 class="card-title">Problem Solving</h5>
+                  <p class="card-text project-text">
+                  A group project, we identified a problem on campus and
+                  came up with a simple solution to it.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+      </section>
+    );
+  }
 }
 export default Projects;
